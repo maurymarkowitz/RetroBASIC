7e80ef63ecb1f90be13f44f773cc2215330c5e7f
Maury Markowitz <maury.markowitz@gmail.com>
2022-08-10 14:43:25 -0400

    first (semi-)working version

--- src/list.c
+++ src/list.c
@@ -21,23 +21,24 @@ Boston, MA 02111-1307, USA.  */
 #include <string.h>
 #include "list.h"
 
-/**
- * Creates an empty list node.
+/*
+ * Creates an empty list node. Private method.
  */
-list_t* lst_alloc() {
-  list_t *list = (list_t *)malloc(sizeof(list_t));
-  if(list == NULL)
+list_t* _lst_alloc(void);
+list_t* _lst_alloc() {
+  list_t *node = (list_t *)malloc(sizeof(list_t));
+  if(node == NULL)
     return NULL;
   
-  list->data = NULL;
-  list->key = NULL;
-  list->next = NULL;
-  list->prev = NULL;
+  node->data = NULL;
+  node->key = NULL;
+  node->next = NULL;
+  node->prev = NULL;
   
-  return list;
+  return node;
 }
 
-/**
+/*
  * Removes and frees the list itself. The user has to free the items within first!
  */
 void lst_free(list_t *list)
@@ -57,7 +58,7 @@ void lst_free(list_t *list)
   }
 }
 
-/**
+/*
  * Removes and frees the entire list and the user data within.
  */
 void lst_free_everything(list_t *list)
@@ -89,7 +90,7 @@ void lst_free_everything(list_t *list)
   // TODO: at this point the original list still has one invalid node in it, and will thus say length 1
 }
 
-/**
+/*
  * Returns the length of the list.
  */
 int lst_length(list_t *list)
@@ -105,26 +106,7 @@ int lst_length(list_t *list)
   return length;
 }
 
-list_t* lst_next(list_t *list)
-{
-  if(list == NULL)
-    return list;
-
-  return list->next;
-}
-
-/**
- * Returns the previous node, which may be NULL.
- */
-list_t* lst_previous(list_t *list)
-{
-  if(list == NULL)
-    return list;
-
-  return list->prev;
-}
-
-/**
+/*
  * Returns the first node.
  */
 list_t* lst_first(list_t *list)
@@ -140,7 +122,7 @@ list_t* lst_first(list_t *list)
   return node;
 }
 
-/**
+/*
  * Returns the last node.
  */
 list_t* lst_last(list_t *list)
@@ -156,7 +138,7 @@ list_t* lst_last(list_t *list)
   return node;
 }
 
-/**
+/*
  * Returns the node for a given data item.
  */
 list_t* lst_item(list_t *list, void* data)
@@ -165,11 +147,11 @@ list_t* lst_item(list_t *list, void* data)
     return list;
 
   list_t* node = lst_first(list);
-  if(node == NULL) return NULL;
+  if(node == NULL)
+    return NULL;
   
-  while(node->data != data && node->next != NULL) {
+  while(node->data != data && node->next != NULL)
     node = node->next;
-  }
   
   // only return it if its in the list, if we hit the end return null
   if(node->data == data)
@@ -178,7 +160,7 @@ list_t* lst_item(list_t *list, void* data)
     return NULL;
 }
 
-/**
+/*
  * Returns the node at a given index.
  */
 list_t* lst_item_at(list_t *list, int index)
@@ -202,11 +184,26 @@ list_t* lst_item_at(list_t *list, int index)
   else
     return NULL;
 }
+/*
+ * Returns the node with a given key.
+ */
+list_t* lst_item_with_key(list_t *list, char *key)
+{
+  if(list == NULL)
+    return list;
 
-/**
+  list_t* node = lst_first(list);
+  while(node != NULL && node->key != NULL && strcmp(key, node->key) != 0)
+    node = node->next;
+  
+  return node;
+}
+
+
+/*
  * Returns the index of an item (pointer) or -1 if it's not found.
  */
-int lst_position(list_t *list, void *data)
+int lst_position_of(list_t *list, void *data)
 {
   if(list == NULL)
     return -1;
@@ -228,14 +225,14 @@ int lst_position(list_t *list, void *data)
     return -1;
 }
 
-/**
+/*
  * Adds a value at the end of the given list.
  */
 list_t* lst_append(list_t* list, void *data)
 {
   // always going to produce at least one new node
   list_t *new_list;
-  new_list = lst_alloc();
+  new_list = _lst_alloc();
   new_list->data = data;
   
   // now add it to the end of there were other items already
@@ -256,14 +253,14 @@ list_t* lst_append(list_t* list, void *data)
   }
 }
 
-/**
+/*
  * Adds a value at the begining of the given list.
  */
 list_t* lst_prepend(list_t* list, void *data)
 {
   list_t *new_node;
   
-  new_node = lst_alloc();
+  new_node = _lst_alloc();
   new_node->data = data;
   new_node->next = list;
   
@@ -279,74 +276,75 @@ list_t* lst_prepend(list_t* list, void *data)
   return new_node;
 }
 
-/**
- * Inserts a newly created node from the given value at the given index
+/*
+ * Inserts the data at the given index.
  */
-list_t* lst_insert_after(list_t *list, int index, void *data)
+list_t* lst_insert_at(list_t *list, void *data, int position)
 {
-  // get the existing item at that index
-  list_t* current_node = lst_item_at(list, index);
+  list_t *new_node;
+  list_t *tmp_node;
   
-  // we'll do an insert for any index, even if it's off the end of the list
-  // or the list is empty. the line above returned NULL for either of those,
-  // so if we got a NULL we can simply append the item, which may create the
-  // list if it's new
-  if(current_node == NULL) {
-    current_node = lst_append(list, data);
-    return current_node;
-  }
+  // if we insert before the start, that means the end, zero is the start.
+  if (position < 0)
+    return lst_append(list, data);
+  else if (position == 0)
+    return lst_prepend(list, data);
   
-  // if we did get a good location then the list exists and index is value,
-  // so make the new node...
-  list_t *new_node = lst_alloc();
+  tmp_node = lst_item_at(list, position);
+  if(tmp_node == NULL)
+    return lst_append(list, data);
+  
+  new_node = _lst_alloc();
+  new_node->data = data;
+  new_node->prev = tmp_node->prev;
+  new_node->prev->next = new_node;
+  new_node->next = tmp_node;
+  new_node->prev = new_node;
+
+  return list;
+}
+
+/*
+ * Uses the "key" string to find the proper location to insert new data.
+ */
+list_t* lst_insert_sorted(list_t *list, void *data, char *key)
+{
+  // try to build a new node and fail out otherwise
+  list_t *new_node = _lst_alloc();
   if(new_node == NULL)
     return NULL;
   new_node->data = data;
+  new_node->key = key;
+
+  // if the list is empty we always insert at the front
+  if(list == NULL)
+    return new_node;
   
-  // get the next node too, which may not exist if index is the end of the list
-  list_t *next_node = current_node->next;
+  // get the head of the list and roll forward until we find the right location
+  list_t *node_after = lst_first(list);
+  while(node_after != NULL && node_after->key != NULL && strcmp(key, node_after->key) > 0)
+    node_after = node_after->next;
+  // FIXME: what happens if we need to be after the last node in the list?
+  
+  // get the previous node too, which may not exist if we're at the start
+  list_t *node_before = NULL;
+  if(node_after != NULL)
+    node_before = node_after->prev;
   
   // and link it in, note that we might be at the end
-  current_node->next = new_node;
-  new_node->prev = current_node;
-  if(next_node != NULL) {
-    next_node->prev = new_node;
-    new_node->next = next_node;
+  if(node_after != NULL) {
+    node_after->prev = new_node;
+    new_node->next = node_after;
+  }
+  if(node_before != NULL) {
+    node_before->next = new_node;
+    new_node->prev = node_before;
   }
   
   return new_node;
 }
 
-/**
- * Uses the "key" string to find the proper location to insert new data.
- */
-list_t* lst_insert_sorted(list_t *list, char *key, void *data)
-{
-  // try to build a new node and fail out otherwise
-  list_t *new_node = lst_alloc();
-  if(new_node == NULL)
-    return NULL;
-  
-  // start at the head and find the right location (which may be the head)
-  list_t *prev_node = lst_first(list);
-  while(strcmp(key, prev_node->key) >= 0) {
-    prev_node = prev_node->next;
-  }
-  // get the next node too, which may not exist if index is the end of the list
-  list_t *next_node = prev_node->next;
-  
-  // and link it in, note that we might be at the end
-  prev_node->next = new_node;
-  new_node->prev = prev_node;
-  if(next_node != NULL) {
-    next_node->prev = new_node;
-    new_node->next = next_node;
-  }
-  
-  return new_node;
-}
-
-/**
+/*
  * Copies a list into a new list and returns the new one.
  */
 list_t* lst_copy(list_t *list)
@@ -362,14 +360,14 @@ list_t* lst_copy(list_t *list)
     list = lst_first(list);
   
   // got memory?
-  list_t *new_list = lst_alloc();
+  list_t *new_list = _lst_alloc();
   if(new_list == NULL)
     return NULL;
   new_list->data = list->data;
 
   // do the copy
   while(list->next != NULL) {
-    new_node = lst_alloc();
+    new_node = _lst_alloc();
     new_node->data = list->data;
     new_node->key = list->key;
     lst_append(new_list, new_node);
@@ -379,7 +377,7 @@ list_t* lst_copy(list_t *list)
   return new_list;
 }
 
-/**
+/*
  * Concats two lists
  */
 list_t* lst_concat(list_t *first_list, list_t *second_list)
@@ -395,10 +393,10 @@ list_t* lst_concat(list_t *first_list, list_t *second_list)
   return first_list;
 }
 
-/**
+/*
  * Removes and frees the node pointing to a given @p data
  */
-void* lst_remove(list_t *list, void* data)
+list_t* lst_remove(list_t *list, void* data)
 {
   // get the existing node for that item
   list_t* current_node = lst_item(list, data);
@@ -416,16 +414,16 @@ void* lst_remove(list_t *list, void* data)
     next_node->prev = prev_node;
 
   free(current_node);
-  return data;
+  return list;
 }
 
-/**
+/*
  * Removes and frees the node at the given index and returns the pointer to the associated data
  */
-void* lst_remove_at(list_t *list, int index)
+void* lst_remove_at(list_t *list, int position)
 {
   // get the existing item at that index and fail out if it doesn't exist
-  list_t* current_node = lst_item_at(list, index);
+  list_t* current_node = lst_item_at(list, position);
   if(current_node == NULL)
     return NULL;
 
@@ -444,7 +442,20 @@ void* lst_remove_at(list_t *list, int index)
   return data;
 }
 
-/**
+/*
+ * Calls the provided function on each of the elements in the list.
+ */
+void lst_foreach(list_t *list, void (*function)(void *))
+{
+  list_t *next;
+  while(list != NULL) {
+      next = list->next;
+      (*function)(list->data);
+      list = next;
+    }
+}
+
+/*
  * Alias for prepend, for API purposes
  */
 list_t* lst_push(list_t *list, void *data)
@@ -452,7 +463,7 @@ list_t* lst_push(list_t *list, void *data)
   return lst_prepend(list, data);
 }
 
-/**
+/*
  * Alias for remove_at(0), for API purposes
  */
 void* lst_pop(list_t *list)
