fff83719cc4003ec4907ed25bc617eb7e8b37143
Maury Markowitz <maury.markowitz@gmail.com>
2022-08-16 16:46:10 -0400

    changing the names again because I am a glutton for punishment

--- src/list.c
+++ src/list.c
@@ -18,7 +18,7 @@ along with RetroBASIC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
-#include "lst.h"
+#include "list.h"
 
 /*
  * Creates an empty list node. Private method.
--- src/strng.c
+++ src/strng.c
@@ -18,7 +18,7 @@ along with RetroBASIC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
-#include "str.h"
+#include "strng.h"
 
 /* makes a new string and copies in chars */
 char* str_new(char *string)
