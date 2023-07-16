04de4c13bfe45243ede1fe3587cdce3cdf97d8fd
Maury Markowitz <maury.markowitz@gmail.com>
2023-07-16 10:16:30 -0400

    added new statistic to output the average number of digits in a line number updated version to 1.7.1

--- src/main.c
+++ src/main.c
@@ -27,7 +27,7 @@
 /* simple version info for --version command line option */
 static void print_version()
 {
-  puts("RetroBASIC 1.7");
+  puts("RetroBASIC 1.7.1");
 }
 
 /* usage short form, just a list of the switches */
