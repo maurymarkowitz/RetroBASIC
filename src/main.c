7c40f28bf19fbfc3de25094093d084929f521fcd
Maury Markowitz <maury.markowitz@gmail.com>
2023-07-16 10:18:04 -0400

    no, that should be 1.7.2

--- src/main.c
+++ src/main.c
@@ -27,7 +27,7 @@
 /* simple version info for --version command line option */
 static void print_version()
 {
-  puts("RetroBASIC 1.7.1");
+  puts("RetroBASIC 1.7.2");
 }
 
 /* usage short form, just a list of the switches */
