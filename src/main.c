4c216a4af5a7b13e7f51fc4cbea3ba50939500fa
Maury Markowitz <maury.markowitz@gmail.com>
2023-11-09 10:19:27 -0500

    updating version to 1.8.3

--- src/main.c
+++ src/main.c
@@ -27,7 +27,7 @@
 /* simple version info for --version command line option */
 static void print_version()
 {
-  puts("RetroBASIC 1.8.2");
+  puts("RetroBASIC 1.8.3");
 }
 
 /* usage short form, just a list of the switches */
