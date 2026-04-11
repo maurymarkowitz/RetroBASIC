d721a5e89d1fe982772b439e2b3f4c144d8fe956
maurymarkowitz <maury.markowitz@gmail.com>
2026-04-11 08:54:59 -0400

    still trying...

--- src/stdhdr.h
+++ src/stdhdr.h
@@ -35,8 +35,12 @@ Boston, MA 02111-1307, USA.  */
 #include <limits.h> // gives INT_MAX
 #include <signal.h> // for sig_atomic_t and signal handling
 
-#if defined(__MINGW32__) || defined(__MINGW64__)
+#if defined(_WIN32) || defined(_WIN64) || defined(__MINGW32__) || defined(__MINGW64__)
   #include <io.h>
+  #include <unistd.h>
+  #ifndef STDIN_FILENO
+    #define STDIN_FILENO 0
+  #endif
 #else
   #include <unistd.h>
 #endif
